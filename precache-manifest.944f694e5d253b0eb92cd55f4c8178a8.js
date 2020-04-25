e1770c4839d21e06e7734dee376d3dee283fab8f
thynguyen <imthyins@gmail.com>
2020-04-25 20:10:20 +0700

    Updates

---
+++ precache-manifest.944f694e5d253b0eb92cd55f4c8178a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdba2888f1bc0660fa9d60077c644030",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12153e250b14b921dcde",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "66fd6364fa738c1d2f17",
+    "url": "/static/js/2.c5ffc7a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c5ffc7a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12153e250b14b921dcde",
+    "url": "/static/js/main.4e214b3e.chunk.js"
+  },
+  {
+    "revision": "715cfa5cc6b29c230237",
+    "url": "/static/js/runtime-main.90e38644.js"
+  }
+]);
